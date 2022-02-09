15e11181b5b22b32c2a0f065bac696c52b9d762d
Chandu030819 <chandu.ca1430@gmail.com>
2022-02-09 23:07:18 +0530

    added clone step

--- chandu-private.groovy
+++ chandu-private.groovy
@@ -3,6 +3,8 @@ pipeline {
         stages{
             stage("clone"){
                 steps{
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'git-auth', url: 'https://github.com/Chandu030819/pipeline1.git']]])
+                sh "ls -lart ./*"    
                 println "hera download"
                 }
             }
