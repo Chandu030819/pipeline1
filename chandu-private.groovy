c01c5713300fef69ef81c0204619121ccd778412
Chandu030819 <chandu.ca1430@gmail.com>
2022-02-09 23:39:00 +0530

    build and uoload

--- chandu-private.groovy
+++ chandu-private.groovy
@@ -3,7 +3,7 @@ pipeline {
         stages{
             stage("clone"){
                 steps{
-                checkout([$class: 'GitSCM', branches: [[name: '*/main']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'git-auth', url: 'https://github.com/Chandu030819/pvtrepo03.gitg']]])
+                checkout([$class: 'GitSCM', branches: [[name: '*/main']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'git-auth', url: 'https://github.com/Chandu030819/pvtrepo03.git']]])
                 sh "ls -lart ./*"    
                 println "hera download"
                 }
