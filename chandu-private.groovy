2b44145b60342d5acd235c567a391a4a58a4d87a
Chandu030819 <chandu.ca1430@gmail.com>
2022-02-09 23:16:32 +0530

    add pvt repo

--- chandu-private.groovy
+++ chandu-private.groovy
@@ -3,7 +3,7 @@ pipeline {
         stages{
             stage("clone"){
                 steps{
-                checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'git-auth', url: 'https://github.com/Chandu030819/pipeline1.git']]])
+                checkout([$class: 'GitSCM', branches: [[name: '*/main']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'git-auth', url: 'https://github.com/Chandu030819/pvtrepo03.git']]])
                 sh "ls -lart ./*"    
                 println "hera download"
                 }
